a2c4749a8c5ad2342cc198e34806705245d5884a
Arik Barenboim <arikbarenboim@gmail.com>
2018-03-18 18:26:41 -0400

    Added 2.1 Changing orientation

--- a3_300024372_300016044/Cube.java
+++ a3_300024372_300016044/Cube.java
@@ -5,9 +5,13 @@ public class Cube{
 	}
 
 	private Color[] faces;
+	private final Color[] IDENTITY;
+	private int nbreOfRotations;
 
 	public Cube(Color[] faces){
 		this.faces = faces;
+		this.IDENTITY = new Color[]{faces[0],faces[1],faces[2],faces[3],faces[4],faces[5]};
+		this.nbreOfRotations = 0;
 	}
 
 	public Color getUp(){
@@ -38,10 +42,109 @@ public class Cube{
 		return ("["+ faces[0]+", "+ faces[1]+", "+ faces[2]+", "+ faces[3]+", "+ faces[4]+", "+ faces[5] +"]");
 	}
 
+	public boolean hasNext(){
+		return nbreOfRotations < 24 && nbreOfRotations > -1;
+	}
+
+	private Color[] identity(){
+		return IDENTITY;
+	}
+
+	private void rotate(){
+		Color temp;
+
+		temp = faces[4];
+
+		faces [4] = faces [3];
+		faces [3] = faces [2];
+		faces [2] = faces [1];
+		faces [1] = temp;
+
+	}
+
+	private void rightRoll(){
+		Color temp;
+
+		temp = faces[5];
+
+		faces [5] = faces [2];
+		faces [2] = faces [0];
+		faces [0] = faces [4];
+		faces [4] = temp;
+
+	}
+
+	private void leftRoll(){
+		Color temp;
+
+		temp = faces[5];
+
+		faces [5] = faces [4];
+		faces [4] = faces [0];
+		faces [0] = faces [2];
+		faces [2] = temp;
+
+	}
+
+	public void next(){
+		switch (nbreOfRotations){
+			case 0: identity(); break;
+			case 1: rotate(); break;
+			case 2: rotate(); break;
+			case 3: rotate(); break;
+			case 4: rightRoll(); break;
+			case 5: rotate(); break;
+			case 6: rotate(); break;
+			case 7: rotate(); break;
+			case 8: rightRoll(); break;
+			case 9: rotate(); break;
+			case 10: rotate(); break; 
+			case 11: rotate(); break;
+			case 12: leftRoll(); break;
+			case 13: rotate(); break;
+			case 14: rotate(); break;
+			case 15: rotate(); break;
+			case 16: leftRoll(); break;
+			case 17: rotate(); break;
+			case 18: rotate(); break;
+			case 19: rotate(); break;
+			case 20: rightRoll(); break;
+			case 21: rotate(); break;
+			case 22: rotate(); break;
+			case 23: rotate(); break;
+			default:
+				System.out.println("Whoops Broken");
+				break;
+		}
+		nbreOfRotations++;
+	}
+
+	public void reset(){
+		for (int i = 0; i < IDENTITY.length; i++){
+			faces[i] = IDENTITY[i];
+		}
+		nbreOfRotations = 0;
+	}
+
+
+
 	public static void main(String[] args) {
 		Cube c;
 		c = new Cube(new Color[]{Color.BLUE,Color.GREEN,Color.WHITE,Color.GREEN,Color.BLUE,Color.RED});
-		System.out.println(c);
+
+		c.reset();
+		while (c.hasNext()){
+			c.next();
+			System.out.println(c);
+		}
+
+		System.out.println("reset: ");
+		c.reset();
+		while (c.hasNext()){
+			c.next();
+			System.out.println(c);
+		}
+
 	}
 
 
