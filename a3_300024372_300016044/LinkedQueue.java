56bca3c48377e1128c444cff73f8e2b522fcb507
Arik Barenboim <arikbarenboim@gmail.com>
2018-03-25 23:02:50 -0400

    Done The whole a3

--- a3_300024372_300016044/LinkedQueue.java
+++ a3_300024372_300016044/LinkedQueue.java
@@ -1,5 +1,14 @@
-public class LinkedQueue<E> implements Queue<E> {
 
+/**The LinkedQueue class represents a first-in-first-out (FIFO)
+ *  queue of generic items.
+ *  It supports adding and removing
+ *  operations, along with methods for testing if the queue is empty, 
+ *  and iterating through the items in FIFO order.
+ *  This class contains a static nested class for linked elements.
+ */
+public class LinkedQueue<E> implements Queue<E> {
+    
+    //Helper static linked list class
     private static class Elem<T> {
 
         private T value;
@@ -14,10 +23,22 @@ public class LinkedQueue<E> implements Queue<E> {
     private Elem<E> front;
     private Elem<E> rear;
 
+    /**
+     * Returns the element at the front of the queue
+     * 
+     * @return element at front of queue
+     */
     public E peek() {
         return front.value;
     }
-
+   
+    /**
+     * Adds element to rear of queue
+     * 
+     * @param o
+     *        element to be added to queue
+     * 
+     */
     public void enqueue( E o ) {
         Elem<E> newElem;
         newElem = new Elem<E>( o, null );
@@ -29,7 +50,12 @@ public class LinkedQueue<E> implements Queue<E> {
             rear = newElem;
         }
     }
-
+    
+    /**
+    * Removes and returns the element at the front of the queue
+    * 
+    * @return front element of queue
+    */
     public E dequeue() {
         E result = front.value;
         if ( front != null & front.next == null ) {
@@ -40,6 +66,11 @@ public class LinkedQueue<E> implements Queue<E> {
         return result;
     }
 
+   /**
+   * Tests if the queue is empty
+   * 
+   * @return true if queue is empty, false otherwise
+   */
     public boolean isEmpty() {
         return front == null;
     }
